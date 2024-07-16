9f8010fff89e9317ad83d431ab7c1a05ddf092c8
infinite41i <57639431+infinite41i@users.noreply.github.com>
2024-07-16 19:45:04 +0330

    changed namings and variable types

--- validator.js
+++ validator.js
@@ -26,63 +26,61 @@
     // inverse table inv
     var inv = [0, 4, 3, 2, 1, 5, 6, 7, 8, 9];
   
-  // converts string or number to an array and inverts it
-  function invArray(array) {
-    if (Object.prototype.toString.call(array) == "[object Number]") {
-      array = String(array);
-      console.log(array,"!!!")
+    // converts string or number to an array and inverts it
+    function invArray(array) {
+      if (Object.prototype.toString.call(array) == "[object Number]") {
+        array = String(array);
+        console.log(array,"!!!")
+      }
+      if (Object.prototype.toString.call(array) == "[object String]") {
+        array = array.split("").map(Number);
+        console.log(array,"!!!")
+       
+      }
+      return array.reverse();
     }
-    if (Object.prototype.toString.call(array) == "[object String]") {
-      array = array.split("").map(Number);
-      console.log(array,"!!!")
-     
-    }
-    return array.reverse();
-  }
-  
-  // generates checksum
-  function generate(array) {
     
-    var c = 0;
-    var invertedArray = array.reverse();
-  
-    for (var i = 0; i < invertedArray.length; i++) {
-        console.log(d[c][p[((i + 1) % 8)][invertedArray[i]]])
-      c = d[c][p[((i + 1) % 8)][invertedArray[i]]];
-
-    }
-    return inv[c];
-  }
-  
-  // validates checksum
-  function validate(array) {
-  
-    var c = 0;
-    var invertedArray = invArray(array);
-  
-    for (var i = 0; i < invertedArray.length; i++) {
-      c = d[c][p[(i % 8)][invertedArray[i]]];
-    }
-  
-    return (c === 0);
-  }
-  
-  function validateAadhaar(aadhaarString) {
-    if (aadhaarString.length != 12) {
-      return new Error('Aadhaar numbers should be 12 digit in length');
-    }
-    if (aadhaarString.match(/[^$,.\d]/)) {
-      return new Error('Aadhaar numbers must contain only numbers');
-    }
-    var aadhaarArray = aadhaarString.split('');
-  
-    var toCheckChecksum = aadhaarArray.pop();
+    // generates checksum
+    function generate_checksum(array) {
+      let c = 0;
+      let invertedArray = array.reverse();
     
-    if (generate(aadhaarArray) == toCheckChecksum) {
-      return true;
-    } else {
-      return false;
+      for (let i = 0; i < invertedArray.length; i++) {
+          // console.log(d[c][p[((i + 1) % 8)][invertedArray[i]]])
+        c = d[c][p[((i + 1) % 8)][invertedArray[i]]];
+  
+      }
+      return inv[c];
     }
-  };
-
-
+    
+    // validates checksum
+    function validate(array) {
+    
+      let c = 0;
+      let invertedArray = invArray(array);
+    
+      for (let i = 0; i < invertedArray.length; i++) {
+        c = d[c][p[(i % 8)][invertedArray[i]]];
+      }
+    
+      return (c === 0);
+    }
+    
+    function validateAadhaar(aadhaarString) {
+      if (aadhaarString.length != 12) {
+        return new Error('Aadhaar numbers should be 12 digit in length');
+      }
+      if (aadhaarString.match(/[^$,.\d]/)) {
+        return new Error('Aadhaar numbers must contain only numbers');
+      }
+      let aadhaarArray = aadhaarString.split('');
+    
+      let toCheckChecksum = aadhaarArray.pop();
+      
+      if (generate_checksum(aadhaarArray) == toCheckChecksum) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+  
